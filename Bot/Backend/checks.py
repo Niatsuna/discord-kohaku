a353aa880cf544642577920f3e28f58cfaf663ec
Niatsuna <niatsuna@gmail.com>
2020-08-24 16:40:31 +0200

    Added check_is_owner

--- Bot/Backend/checks.py
+++ Bot/Backend/checks.py
@@ -1,6 +1,7 @@
 ''' CHECKS.py - Stores Bot Checks like 'does a file exist for that use?', 'is this user an admin?' etc. '''
 # > ---------------------------------------------------------------------------
 # > Imports
+from Bot.Backend.constants import OWNER
 from Bot.Backend.utils import json_load
 
 # > Admin
@@ -8,6 +9,10 @@ def check_is_admin(ctx):
     author_id = ctx.message.author.id
     json = json_load('Bot/Resources/json/admin.json')
     return author_id == json['owner'] or author_id in json['admins']
+
+def check_is_owner(ctx):
+    author_id = ctx.message.author.id
+    return author_id == OWNER
 
 # > Secret
 def check_is_secret(cog):
