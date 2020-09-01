941d4796c3a665dc6c26ec4dec2625ac32a6eeac
Niatsuna <niatsuna@gmail.com>
2020-09-01 02:45:15 +0200

    Added Icons for all Cogs

--- Bot/Backend/constants.py
+++ Bot/Backend/constants.py
@@ -35,6 +35,7 @@ GITHUB_URL_BOARD = 'https://github.com/Niatsuna/discord-kohaku/projects/1'
 # > Cog : Animal Crossing New Horizons
 AC_REST_API = 'https://acnhapi.com/v1/'
 AC_WIKI_URL = 'https://nookipedia.com/wiki/'
+AC_ICON_URL = 'https://cdn.discordapp.com/attachments/270376999925579778/744274811559215165/Du_bist_so_ein_Jfif_nea_das_musste_mal_gesagt_werden.png' # Love ya, hon
 
 # > Cog : Dead by Daylight
 DBD_REST_API = 'https://dbd-stats.info/api/'
@@ -44,14 +45,18 @@ DBD_ICON_URL = 'https://cdn.discordapp.com/attachments/737974066362712094/739284
 # > Cog : Fate/Grand Order
 FGO_REST_API = 'https://api.atlasacademy.io/nice/NA/'
 FGO_WIKI_URL = 'https://grandorder.wiki/'
+FGO_ICON_URL = 'https://cdn.discordapp.com/attachments/626462024913911818/750153394949193848/NeroIcon.png'
 
 # > Cog : Never Have I Ever
 NHIE_URL = 'http://www.neverhaveiever.org'
+NHIE_ICON_URL = 'https://discordapp.com/assets/4e4a4ac66710b0aecf931cd72cd65d9e.svg'
 
 # > Cog : Pokémon
 PKM_REST_API = 'https://pokeapi.co/api/v2/'
 PKM_WIKI_URL_DE = 'https://www.pokewiki.de/'
 PKM_WIKI_URL_EN = 'https://www.pokemon.com/us/pokedex/'
+PKM_ICON_URL = 'https://cdn.discordapp.com/emojis/741978619361886261.png?v=1'
 
 # > Cog : Would you rather
 WYR_URL = 'http://either.io/{}'
+WYR_ICON_URL = 'https://discordapp.com/assets/2cc6266229c7e2ccfde10e81782e8b5c.svg'
