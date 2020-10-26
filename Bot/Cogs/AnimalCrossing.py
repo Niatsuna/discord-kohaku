389bcca7f4324bbbbd5ced6565f27a66b9d328e1
Niatsuna <niatsuna@gmail.com>
2020-10-26 01:33:05 +0100

    [WIP] ACNH

--- Bot/Cogs/AnimalCrossing.py
+++ Bot/Cogs/AnimalCrossing.py
@@ -5,7 +5,7 @@ import discord
 from discord.ext import commands
 import Bot.Backend.constants as constants
 import Bot.Backend.utils as utils
-from math import ceil
+from concurrent.futures import ThreadPoolExecutor
 
 ALIASES = ['ac', 'acnh']
 
@@ -15,15 +15,14 @@ class AnimalCrossing(commands.Cog):
     def __init__(self, client):
         self.client = client
         self.instant = {
-            'art'   : self.instant_art(),
-            'song'  : self.instant_songs(),
-            'songs' : self.instant_songs(),
+            'art'       : lambda self, ctx : self.instant_art(ctx),
+            'fossils'   : lambda self, ctx : self.instant_fossils(ctx),
+            'songs'     : lambda self, ctx : self.instant_songs(ctx),
         }
+        self.functions = [self.search_fossil]
         self.key = '1A1o0oesDUh5PCzu42UA47nYOfUaoFaRTNnEhnmsDdCw'
         self.sheets = ['AC - Art', 'AC - Bug', 'AC - Fish', 'AC - Fossils', 'AC - Sea creatures', 'AC - Songs', 'AC - Villagers']
-        self.resource = None
-
-        self.update()
+        self.resource = utils.json_load('Bot/Resources/json/animal_crossing.json')
 
     def help(self, footer):
         title = 'Help Info: {}animalcrossing'.format(constants.INVOKE)
@@ -31,8 +30,7 @@ class AnimalCrossing(commands.Cog):
             alias = '**Aliases: ** {}\n'.format(' '.join(['`{}`'.format(x) for x in ALIASES]))
         else:
             alias = ''
-        possibilities = ['`<art>` - Shows the difference between fake and real', '`<fish/bug/sea creature/fossil>` - Information about named critter', '`<month>` `(south)` - List critters of the month (leavin, arriving and currently catchable)', 
-            '`all year` - List critters which are catchable all year long', '`<song>` - Short preview of the named song', '`songs` - List of all songs', '`<villager>` - Information about named villager']
+        possibilities = ['']
         description = 'This command is connected to the game \'Animal Crossing : New Horizons\' and can show information regarding this game.\n\n{}**Usage: ** \n{}'.format(
             alias, '\n'.join(['`{}animalcrossing` {}'.format(constants.INVOKE, x) for x in possibilities]))
         return utils.embed_create(title=title, description=description, footer=footer)
@@ -43,8 +41,16 @@ class AnimalCrossing(commands.Cog):
     @commands.command(pass_context=True, aliases=ALIASES)
     async def animalcrossing(self, ctx, *, param):
         param = param.lower().strip()
-        
-        await utils.embed_send(ctx, utils.embed_create(title='Ping : {}ms'.format(ceil(self.client.latency*1000))))
+        if self.resource == None:
+            await self.update()
+        if param in self.instant.keys():
+            if not await self.instant[param](self, ctx):
+                await utils.embed_send(ctx, constants.ERROR_WHOOPS)
+        else:
+            with ThreadPoolExecutor(max_workers=len(self.functions)) as executor:
+                _bool = [await executor.submit(utils.map_function_async, [func, ctx, param]).result() for func in self.functions]
+            if True not in _bool:
+                await utils.embed_send(ctx, constants.ERROR_SEARCH_FAIL)
 
     @animalcrossing.error
     async def animalcrossing_error(self,ctx, error):
@@ -59,19 +65,38 @@ class AnimalCrossing(commands.Cog):
         utils.json_store('Bot/Resources/json/animal_crossing.json', result)
         self.resource = result
 
-
-    async def instant_art(self):
-        artworks = self.resource['AC - Art']
-        title = 'Artworks ({})'.format(len(artworks))
+    async def _instant_list(self, ctx, array, title):
+        desc = 'There are currently {} {}.'.format(len(array), title.lower())
         fields = []
-        for i in range(0, len(artworks)):
-            entry = artworks[i]
-            if i % 2 == 0 and i > 0:
-                fields.append([None, True])
-            fields.append(['{} | {}'.format(entry['name'], entry['name_de']), constants.EMPTY_CHAR, True])
-        return False
+        for entry in array:
+            fields.append([entry['name'], '**German:** {}'.format(entry['name_de']), True])
+        await utils.embed_send(ctx, utils.embed_create(title=title, description=desc, fields=fields))
 
-    async def instant_songs(self):
+    async def instant_art(self, ctx):
+        artworks = self.resource['AC - Art']
+        try:
+            await self._instant_list(ctx, artworks, 'Artworks')
+            return True
+        except:
+            return False
+
+    async def instant_songs(self, ctx):
+        songs = self.resource['AC - Songs']
+        try:
+            await self._instant_list(ctx, songs, 'Songs')
+            return True
+        except:
+            return False
+
+    async def instant_fossils(self, ctx):
+        fossils = self.resource['AC - Fossils']
+        try:
+            await self._instant_list(ctx, fossils, 'Fossils')
+            return True
+        except:
+            return False
+
+    async def search_fossil(self, ctx, name):
         return False
 
 
